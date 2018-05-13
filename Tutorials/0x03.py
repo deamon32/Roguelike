502e96d7da0fdb44926f108357c5a707001c0eaf
BuildTools <unconfigured@null.spigotmc.org>
2018-05-13 17:09:52 +0200

    Chapter 3 commit

---
+++ Tutorials/0x03.py
@@ -0,0 +1,18 @@
+import random
+
+'''
+Playing with random built in function a bit
+'''
+
+
+# Pseudorandom numbers each time the program is run
+for x in range(10):
+    print(random.random())
+
+print('-'*40)
+
+# Since we use the same seed, these 10 numbers will always be the same when the program is run
+
+random.seed(1)
+for x in range(10):
+    print(random.random())
